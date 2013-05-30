49e1cd3d15f69965a0cb16ba6f4cd3b3e481f1e3
Storoshook <storoshook@Gmail.com.com>
2013-05-30 19:18:17 +0300

    chanch word file

--- GuessWord/guessword/controllers/word.py
+++ GuessWord/guessword/controllers/word.py
@@ -1,4 +1,5 @@
-import logging
+import logging, simplejson
+
 
 from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
@@ -15,3 +16,8 @@ class WordController(BaseController):
         # or, return a string
         c.teamname = 'LV-087'
         return render('/custompage.html')
+        
+    def json(self):
+        json_apps = '{apps: [app1, app2, app3], ss: 7}'
+        response.headers['Content-type'] = 'application/json'
+        return simplejson.dumps(json_apps, sort_keys=True, indent=4 * ' ')
