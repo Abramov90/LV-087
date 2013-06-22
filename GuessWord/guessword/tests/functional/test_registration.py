4b3654909da24a8ebdc7e1df6f168058830a4c03
irushtc <iryna.rushchyshyn@gmail.com>
2013-06-22 18:45:25 +0300

    Test controller automatically created

---
+++ GuessWord/guessword/tests/functional/test_registration.py
@@ -0,0 +1,7 @@
+from guessword.tests import *
+
+class TestRegistrationController(TestController):
+
+    def test_index(self):
+        response = self.app.get(url(controller='registration', action='index'))
+        # Test response...
