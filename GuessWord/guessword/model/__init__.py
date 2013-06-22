d2c48484965608b51014cabed6eb405158fe3dda
irushtc <iryna.rushchyshyn@gmail.com>
2013-06-22 18:35:52 +0300

    Added a User model

--- GuessWord/guessword/model/__init__.py
+++ GuessWord/guessword/model/__init__.py
@@ -1,16 +1,29 @@
 """The application's model objects"""
+from sqlalchemy.schema import Column
+from sqlalchemy.types import Integer, String, Date, Unicode
+from sqlalchemy import Unicode
 from guessword.model.meta import Session, Base
-
 
 def init_model(engine):
     """Call me before using any of the tables or classes in the model"""
     Session.configure(bind=engine)
 
-    global AppsData
-    class AppsData(Base):
-        """Creates SQLAlchemy model for the table AppsDataTable"""
-        __tablename__ = 'AppsDataTable'
-        __table_args__ = {
-            'autoload': True,
-            'autoload_with': engine
-            }
+    global User
+    class User(Base):
+        '''Establishe the connection with a user table in DB
+        '''
+        __tablename__ = 'user'
+        
+        ID       = Column(Integer, primary_key=True)
+        Login    = Column(Unicode)
+        Password = Column(Unicode)
+        Email    = Column(String)
+        DOB      = Column(Date)
+        Location = Column(Unicode)
+
+        def __init__(self, login, password, email, DOB, location):
+            self.Login    = login
+            self.Password = password
+            self.Email    = email
+            self.DOB      = DOB
+            self.Location = location
